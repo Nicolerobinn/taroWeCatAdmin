cee8882884440b897112d91954e64009c34c789a
cxt <cweb130@163.com>
2021-06-29 15:56:36 +0800

    feat: 新增权限检查自定义指令

--- push.sh
+++ push.sh
@@ -4,7 +4,6 @@ set -e
 git init
 git add -A
 git commit -m '🎉deploy'
-git push -f "https://${access_token}@github.com/chuzhixin/vue-admin-beautiful.git" master
 exec /bin/bash
 
 
--- src/main.js
+++ src/main.js
@@ -4,20 +4,41 @@ import store from './store'
 import router from './router'
 import './plugins'
 import '@/layouts/export'
+
+
+
+/**
+ * @author cxt （不想保留author可删除）
+ * @description vue自定义指令检查权限
+ * @param value
+ * @returns {boolean}
+ */
+Vue.directive('permission', {
+  inserted(el, binding) {
+    const {value} = binding
+    const roles = store.getters['user/permissions']
+    if (value && value instanceof Array && value.length > 0) {
+      const permissionRoles = value
+      const hasPermission = roles.some(role =>permissionRoles.includes(role))
+      if (!hasPermission) {
+        el.parentNode && el.parentNode.removeChild(el)
+      }
+    } else {
+      throw new Error('没有输入权限，默认全部开放')
+    }
+  },
+
+})
+
 /**
  * @author cxt （不想保留author可删除）
  * @description 生产环境默认都使用mock，如果正式用于生产环境时，记得去掉
  */
 
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('@/utils/static')
-  mockXHR()
-}
-
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#vue-admin-beautiful',
+  el: '#vue-admin',
   router,
   store,
   render: (h) => h(App),
